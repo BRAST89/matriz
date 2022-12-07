d0afe58b9c5e7ebabc17f3fea7c08ad25764df66
BRAST89 <100061375+BRAST89@users.noreply.github.com>
2022-12-07 00:56:52 -0500

    Agregar archivos de proyecto.

---
+++ matriz/Ejercicio_21.vb
@@ -0,0 +1,55 @@
+﻿Public Class Ejercicio_21
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim fila, columna As Byte
+        Dim i As Byte
+        fila = Val(TextBox1.Text)
+        columna = Val(TextBox2.Text)
+        matriza.RowCount = fila
+        matriza.ColumnCount = columna
+        For i = 0 To columna - 1
+            matriza.Columns(i).HeaderText = i + 1
+            matriza.Columns(i).Width = 50
+
+        Next
+        matrizb.RowCount = fila
+        matrizb.ColumnCount = columna
+        For i = 0 To columna - 1
+            matrizb.Columns(i).HeaderText = i + 1
+            matrizb.Columns(i).Width = 50
+
+        Next
+        matrizc.RowCount = fila
+        matrizc.ColumnCount = columna
+        For i = 0 To columna - 1
+            matrizc.Columns(i).HeaderText = i + 1
+            matrizc.Columns(i).Width = 50
+
+        Next
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim i, j, k As Byte
+        Dim valor As Integer
+        Dim fila, columna As Byte
+        fila = Val(TextBox1.Text)
+        columna = Val(TextBox2.Text)
+        If fila = columna Then
+            For k = 0 To fila - 1
+                For i = 0 To columna - 1
+                    valor = 0
+                    For j = 0 To columna - 1
+                        valor = valor + Val(matriza.Rows(k).Cells(j).Value) * Val(matrizb.Rows(j).Cells(i).Value)
+                        matrizc.Rows(k).Cells(i).Value = valor
+                    Next
+                Next
+            Next
+        Else
+            MsgBox("la matrz no es cuadrada")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Hide()
+    End Sub
+End Class
---
+++ matriz/conversoru.vb
@@ -0,0 +1,19 @@
+﻿Public Class Ejercicio_7
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Hide()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox4.Text = TextBox1.Text * 1024
+        TextBox3.Text = TextBox1.Text * 1073741824
+        TextBox2.Text = TextBox1.Text * 8000000000
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox1.Clear()
+        TextBox4.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+    End Sub
+End Class
